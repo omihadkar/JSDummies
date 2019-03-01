5c462a197d4784b37b56d455f7356c334270f6d1
omihadkar <omihadkar@gmail.com>
2019-03-01 08:15:45 +0000

    Added express samples

--- expressJsDummies.js
+++ expressJsDummies.js
@@ -1,10 +1,28 @@
 var express= require('express');
+var bodyParser= require('body-parser');
 
 var app= express();
+
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/',function(req,res){
     res.send('this is the homepage.');
 });
+
+app.get('/contact',function(req,res){
+    res.send('this is the homepage.');
+});
+
+app.post('/login', urlencodedParser, function (req, res) {
+    console.log('In login');
+    if (!req.body) return res.sendStatus(400)
+
+    res.send('welcome, ' + req.body)
+  });
+
+
 
 app.listen(3000);
 console.log('Got it..')
